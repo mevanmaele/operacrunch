bb11ef63cb169af55fab947c5077b403dc5807e6
mevanmaele <mevanmaele@gmail.com>
2018-06-09 23:05:52 -0400

    get routing working

--- server/server.js
+++ server/server.js
@@ -5,8 +5,11 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+app.get('/', (req, res) => res.sendFile('index.html', {root: __dirname + '/client/'}))
+
 app.start = function() {
   // start the web server
+
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
